48d388ce767533739cab7dd701fcf63425c0060f
Brendon LeCount <brendon.lecount@gmail.com>
2020-02-24 12:58:55 -0800

    Scene switcher

---
+++ Assets/UI/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			Application.Quit();
+		}
+		else if (Input.GetButtonDown("Scene00"))
+		{
+			SceneManager.LoadScene(0);
+		}
+		else if (Input.GetButtonDown("Scene01"))
+		{
+			SceneManager.LoadScene(1);
+		}
+	}
+
+}
